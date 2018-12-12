32354a68978d2fa995ae38c5ce6f081112433c45
Walker <walker@incuca.com.br>
2018-12-12 19:03:28 -0200

    feat(plugin): add options dynamic module

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const preparator = require('./utils/preparator');
 const getEnhancers = require('./utils/get-enhancers');
+const getModuleFn = require('./utils/get-module-fn');
 
 const docsBaseDir = path.resolve(__dirname, 'fixtures');
 const doPrepare = preparator(docsBaseDir);
@@ -23,6 +24,21 @@ describe('Pass', () => {
       expect(enhancer).toMatchObject({ items: expect.any(Array) });
       expect(enhancer.items).toContainEqual(
         expect.objectContaining({ value: enhancerSrc }),
+      );
+    });
+  });
+
+  it('should add vuepress-pass-options.js module', async () => {
+    const docs = await doPrepare();
+    docs.forEach(({ context }) => {
+      const modules = getModuleFn(context).appliedItems;
+      expect(modules).toContainEqual(
+        expect.objectContaining({
+          value: {
+            name: 'vuepress-pass-options.js',
+            content: expect.stringMatching(/^export const OPTIONS = /),
+          },
+        }),
       );
     });
   });
--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const path = require('path');
 
 module.exports = (options, context) => ({
   enhanceAppFiles: [path.resolve(__dirname, 'lib/enhancer.js')],
+  clientDynamicModules: () => ({
+    name: 'vuepress-pass-options.js',
+    content: `export const OPTIONS = ${JSON.stringify(options)}`,
+  }),
 });
