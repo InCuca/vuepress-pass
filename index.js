075d8fef6eb737ba20af8a15b590074686c6b7c8
Walker <walker@incuca.com.br>
2019-04-29 17:04:25 -0300

    fix(index): fix dynamic module name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = (options, context) => ({
   enhanceAppFiles: [path.resolve(__dirname, 'lib/enhancer.js')],
   clientDynamicModules: () => ({
-    name: 'vuepress-pass.js',
+    name: 'vuepress-pass',
     content: `export const OPTIONS = ${JSON.stringify(options)}`,
   }),
 });
